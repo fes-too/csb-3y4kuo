90390fc1bcf8ab6cb3727c502bddf01a9d4c1a53
Code Sandbox <hello@codesandbox.io>
2022-06-29 07:22:03 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.eb0e2a4dc1c71a910c4c7faf526d3dfd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5afc480272dcb041deb6caabe56f0c46",
+    "url": "https://fes-too.github.io/csb-3y4kuo/index.html"
+  },
+  {
+    "revision": "56bf07d2386578ac069e",
+    "url": "https://fes-too.github.io/csb-3y4kuo/static/js/2.afccd033.chunk.js"
+  },
+  {
+    "revision": "d18eead45ead3ae753dc",
+    "url": "https://fes-too.github.io/csb-3y4kuo/static/js/main.04110e1b.chunk.js"
+  },
+  {
+    "revision": "dede2ffb6b76ccb32cfd",
+    "url": "https://fes-too.github.io/csb-3y4kuo/static/js/runtime-main.43bc1621.js"
+  }
+]);
